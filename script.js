bbf2ed090cd7c3f2cc815ddf0b0163a80fca81a3
Elizabeth Dahab <103453415+ecdahab@users.noreply.github.com>
2024-03-14 11:05:49 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+let a=Number(prompt("Informe um numero:"))
+  b=a%2
+  if(b==0)
+  {
+    alert("Par")
+  }
+  else
+  {
+    alert("Impar")
+  }
+
+
+c=(99*Math.random())+1
+c=Math.trunc(c)
+console.log(c)
+d=c%2
+if(b==1 && d==1)
+  {
+    alert("GANHAR")
+  }
+  else if(b==0 && d==1)
+  {
+    alert("PERDEU")
+  }
+
